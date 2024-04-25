780b0a7eeb8fd338f7d1b67e580c5eef7787eedd
ger-garcia <136447384+ger-garcia@users.noreply.github.com>
2024-04-25 18:52:13 +0200

    42

--- GetNextLine/get_next_line.c
+++ GetNextLine/get_next_line.c
@@ -12,33 +12,58 @@
 
 #include "get_next_line.h"
 
-char *get_next_line(int fd)
+// Function to read data from the file and append it to partial content.
+
+static char *read_from_file(int fd)
 {
     int bytes_read;
-    char *buffer;
+    char *cup_buffer;
+    static int count = 1;
 
-    buffer = ft_calloc(3 + 1, sizeof(char));
-    if (buffer == NULL)
+    printf("ft_calloc#[%d]---", count++);
+    cup_buffer = ft_calloc(3 + 1, sizeof(char));
+    if (cup_buffer == NULL)
         return (NULL);
-    bytes_read = read(fd, buffer, 3);
+    bytes_read = read(fd, cup_buffer, 3);
     if (bytes_read <= 0)
-        return (NULL);
-    return (buffer);
+        return (free(cup_buffer), NULL);
+    return (cup_buffer);
+}
+
+// The get_next_line function to get the next line from the file descriptor.
+
+char *get_next_line(int fd)
+{
+    char *basin_buffer;
+
+    basin_buffer = read_from_file(fd);
+    return(basin_buffer);
 }
 
 int main(void)
 {
-    int fd; // la fuente
-    char *next_line;
-    int count;
+ int  fd;
+ char *line;
+ int  count;
 
-    count = 0;
-    fd = open("./txtfiles/large.txt", O_RDONLY);
-    next_line = get_next_line(fd);
-    count++;
-    printf("[%d]:%s\n", count, next_line); // count is to show you the line numbers
-    next_line = NULL;
-
-    close(fd);
-    return (0);
+ count = 0;
+ fd = open("./txtfiles/new.txt", O_RDONLY);
+ if (fd == -1)
+ {
+  printf("Error opening file");
+  return (1);
+ }
+ while (1)
+ {
+  line = get_next_line(fd);
+  if (line == NULL)
+   break ;
+  count++;
+  printf("[%d]:%s\n", count, line);
+  free(line);
+  line = NULL;
+ }
+ printf("%d\n",BUFFER_SIZE);
+ close(fd);
+ return (0);
 }
