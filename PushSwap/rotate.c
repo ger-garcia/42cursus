a8564a0ff16aa8b1adbc6152da00e8246d4e713f
ger-garcia <136447384+ger-garcia@users.noreply.github.com>
2024-05-31 05:02:46 +0200

    PushINIt

---
+++ PushSwap/rotate.c
@@ -0,0 +1,17 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   rotate.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gergarci <gergarci@student.42barcel>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/30 23:44:43 by gergarci          #+#    #+#             */
+/*   Updated: 2024/05/30 23:44:45 by gergarci         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+void	rotate_a(t_list **stack_a);
+void	rotate_b(t_list **stack_b);
+void	rr_rotate(t_list **stack_a, t_list **stack_b);
